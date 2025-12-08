89045ccaa6302ab026aecdb0920357745dc1f1ee
Manoj Badshah <jsr.uk.coder@gmail.com>
2025-12-08 12:20:18 +0530

    applying auto complete off

--- client/src/pages/Signin.tsx
+++ client/src/pages/Signin.tsx
@@ -91,6 +91,7 @@ const Signin: React.FC = () => {
                     placeholder="••••••••"
                     value={formData.password}
                     onChange={handleChange}
+                    autoComplete="off"
                     className="w-full px-4 py-3 border border-gray-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition-all duration-200 bg-gray-50 focus:bg-white"
                     required
                   />
